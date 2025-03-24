ebbb34994bd0507a8390ebaea1eec0f3886ea9bc
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-03-24 15:30:36 +0530

    Feat: create a login function and login api

--- todo_app/app/Http/Controllers/AuthController.php
+++ todo_app/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -22,5 +23,25 @@ class AuthController extends Controller
         ]);
 
         return response()->json(['message' => 'User registered successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        return response()->json([
+            'token' => $user->createToken('auth_token')->plainTextToken,
+        ]);
     }
 }
