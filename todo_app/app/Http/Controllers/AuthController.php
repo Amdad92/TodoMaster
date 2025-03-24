187c3abbc73310bc37bf29446f5f597a41de70cf
Amdad92 <emdadctg92@gmail.comgit config --global user.email>
2025-03-24 15:21:08 +0530

    Feat: create a register function and register api

--- todo_app/app/Http/Controllers/AuthController.php
+++ todo_app/app/Http/Controllers/AuthController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
 }
